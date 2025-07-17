356b535b65b17e879694e411545f600afde3b5f6
gaetanopyro <gaetano.miscio@gmail.com>
2025-07-17 14:52:25 +0200

    add commit

---
+++ src/test/java/gaetanomiscio/U5_W1_D4/U5W1D4ApplicationTests.java
@@ -0,0 +1,13 @@
+package gaetanomiscio.U5_W1_D4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W1D4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
